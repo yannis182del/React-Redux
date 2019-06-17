5dd7f90637d1ddd4595cb3f637fcdbddc54c9c97
Yannis <Yannis182@outlook.fr>
2019-06-17 21:16:34 +0200

    Started mapStateToProps with react-redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import TodoForm from "./Components/TodoForm";
 import TodoList from "./Components/TodoList";
 
@@ -8,11 +9,17 @@ class App extends Component {
   render() {
     return (
       <div className="Todo-app">
-        <TodoForm currentTodo={this.props.currentTodo} changeCurrent={this.props.changeCurrent} />
+        <TodoForm
+          currentTodo={this.props.currentTodo}
+          changeCurrent={this.props.changeCurrent}
+        />
         <TodoList todos={this.props.todos} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => state
+const ConnectedApp = connect(mapStateToProps)(App);
+
+export default ConnectedApp;
