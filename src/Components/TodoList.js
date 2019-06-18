14a9fb611bc1f942e6782cb0c33252a6c49d0723
Yannis <yannis@jonass-mbp.lan>
2019-06-18 22:50:34 +0200

    Dispatch Asynchronous Actions with Redux Thunk Middleware

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
+import { fetchTodos } from "../reducers/todo";
 
 const TodoItem = ({ isComplete, name }) => (
   <li>
@@ -7,14 +8,24 @@ const TodoItem = ({ isComplete, name }) => (
   </li>
 );
 
-const todoList = props =>(
-  <div className="todo-list">
-    <ul>
-      {props.todos.map(todo => (
-        <TodoItem key={todo.id} {...todo} />
-      ))}
-    </ul>
-  </div>
-);
+class todoList extends Component {
+  componentDidMount() {
+    this.props.fetchTodos();
+  }
+  render() {
+    return (
+      <div className="todo-list">
+        <ul>
+          {this.props.todos.map(todo => (
+            <TodoItem key={todo.id} {...todo} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
-export default connect((state) => ({ todos: state.todos }))(todoList);
+export default connect(
+  state => ({ todos: state.todos }),
+  { fetchTodos }
+)(todoList);
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,21 +1,25 @@
 // The initial state of my App
+import { getTodos } from "../lib/todoServices";
 
 const initialState = {
-  todos: [
-    { id: 1, name: "Create Static UI", isComplete: true },
-    { id: 2, name: "Create Initial State", isComplete: true },
-    { id: 3, name: "Use state to render UI", isComplete: true }
-  ],
-  currentTodo: "",
+  todos: [],
+  currentTodo: ""
 };
 
 const TODO_ADD = "TODO_ADD";
+const TODO_LOAD = " TODO_LOAD";
 const CURRENT_UPDATE = "CURRENT_UPDATE";
-
-
 
 export const updateCurrent = val => ({ type: CURRENT_UPDATE, payload: val });
 
+export const loadTodos = (todos) => ({type: TODO_LOAD, payload: todos})
+
+export const fetchTodos = () => {
+  return (dispatch) => {
+    getTodos()
+    .then(todos => dispatch(loadTodos(todos)));
+  };
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
@@ -23,6 +27,8 @@ export default (state = initialState, action) => {
       return { ...state, todos: state.todos.concat(action.payload) };
     case CURRENT_UPDATE:
       return { ...state, currentTodo: action.payload };
+      case TODO_LOAD:
+          return { ...state, todos: action.payload };
     default:
       return state;
   }
