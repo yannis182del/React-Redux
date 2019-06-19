e7e34d04ab4893b85f6f085907cb9784a48a3705
Yannis <yannis@jonass-mbp.lan>
2019-06-19 21:08:28 +0200

    Save new records on the server with redux thunk and fetch

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import { updateCurrent } from "../reducers/todo";
+import { updateCurrent, saveTodo } from "../reducers/todo";
 
-const TodoForm = props => {
-  const { currentTodo, updateCurrent } = props;
-  const handleInputChange = evt => {
+class TodoForm extends Component {
+  handleInputChange = evt => {
     const val = evt.target.value;
-    updateCurrent(val);
+    this.props.updateCurrent(val);
   };
-  return (
-    <form>
-      <input type="text" value={currentTodo} onChange={handleInputChange} />
-    </form>
-  );
-};
+
+  handleSumbit = e => {
+    e.preventDefault();
+    this.props.saveTodo(this.props.currentTodo);
+  };
+
+  render() {
+    const { currentTodo } = this.props;
+    return (
+      <form onSubmit={this.handleSumbit}>
+        <input
+          type="text"
+          value={currentTodo}
+          onChange={this.handleInputChange}
+        />
+      </form>
+    );
+  }
+}
 
 export default connect(
   state => ({ currentTodo: state.currentTodo }),
-  { updateCurrent }
+  { updateCurrent, saveTodo }
 )(TodoForm);
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,5 +1,5 @@
 // The initial state of my App
-import { getTodos } from "../lib/todoServices";
+import { getTodos, createTodo } from "../lib/todoServices";
 
 const initialState = {
   todos: [],
@@ -11,24 +11,33 @@ const TODO_LOAD = " TODO_LOAD";
 const CURRENT_UPDATE = "CURRENT_UPDATE";
 
 export const updateCurrent = val => ({ type: CURRENT_UPDATE, payload: val });
-
-export const loadTodos = (todos) => ({type: TODO_LOAD, payload: todos})
+export const loadTodos = todos => ({ type: TODO_LOAD, payload: todos });
+export const addTodo = todo => ({ type: TODO_ADD, payload: todo });
 
 export const fetchTodos = () => {
-  return (dispatch) => {
-    getTodos()
-    .then(todos => dispatch(loadTodos(todos)));
+  return dispatch => {
+    getTodos().then(todos => dispatch(loadTodos(todos)));
+  };
+};
+
+export const saveTodo = name => {
+  return dispatch => {
+    createTodo(name).then(res => dispatch(addTodo(res)));
   };
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case TODO_ADD:
-      return { ...state, todos: state.todos.concat(action.payload) };
+      return {
+        ...state,
+        currentTodo: "",
+        todos: state.todos.concat(action.payload)
+      };
     case CURRENT_UPDATE:
       return { ...state, currentTodo: action.payload };
-      case TODO_LOAD:
-          return { ...state, todos: action.payload };
+    case TODO_LOAD:
+      return { ...state, todos: action.payload };
     default:
       return state;
   }
